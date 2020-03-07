f229529f9db475df6296089b69fab5f62c9d26ef
Juan Cely <juanscelyg@gmail.com>
2020-03-07 15:01:45 +0100

    5degree version has been added

--- mur_control/src/common/mur_common.py
+++ mur_control/src/common/mur_common.py
@@ -44,8 +44,9 @@ def push_to_pwm(push_value):
     return pwm_value
 
 def pressure_to_meters(pressure):
-    a=-0.010197442;
-    meters=a*pressure;
+    a=-0.10197442;
+    b=10.2903;
+    meters=a*pressure+b;
     return meters
 
 def aruco_to_world(id, rvec, tvec):
